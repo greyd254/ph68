f4c009b745fc4dfb50957ea3186fc4cf8ec922c7
Trần Minh Đức <duc.212954@student.hactech.edu.vn>
2023-03-05 11:13:33 +0700

    ph68
    check bootstrap test

---
+++ trongminhmovies/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+	<main class="col-xs-12 col-sm-12 col-md-12">
+		<div class="post-content panel-body text-center">
+			<h4 class="title-info">404 Not Found!</h4>
+			<p style="font-size: 30px;">Có thể Link phim đã thay đổi, bạn <b style="color: #fff;">tìm tên phim trên ô tìm kiếm</b> nhé.</p>
+			<a href="/" title="Back to home" class="btn btn-primary">Back to home</a>
+		</div>
+	</main>
+<?php get_footer(); ?>
